6363e0452e494faf911595a604589a23d46ea974
eablovatskaya <104693894+eablovatskaya@users.noreply.github.com>
2022-07-17 14:17:49 +0000

    Deploying to gh-pages from @ eablovatskaya/ice-cream1@c5c6ed839f28444195038a2b86967672822f3237 🚀

---
+++ index.4ff28af3.js
@@ -0,0 +1,2 @@
+!function(){var o={openModalBtn:document.querySelector("[data-about-modal-open]"),closeModalBtn:document.querySelector("[data-about-modal-close]"),modal:document.querySelector("[data-about-modal]"),body:document.querySelector("body")};function e(){o.modal.classList.toggle("is-hidden"),o.body.classList.toggle("no-scroll")}o.openModalBtn.addEventListener("click",e),o.closeModalBtn.addEventListener("click",e)}();
+//# sourceMappingURL=index.4ff28af3.js.map
